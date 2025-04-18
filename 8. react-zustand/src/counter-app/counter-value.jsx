1ace0cce8c0de4ef002239150ef18ab72433cc7f
ravikesana1998 <ramuawsdevops1@gmail.com>
2025-04-18 11:28:07 +0530

    Delete 8. react-zustand directory

--- 8. react-zustand/src/counter-app/counter-value.jsx
+++
@@ -1,9 +0,0 @@
-import useCounter from "../store/use-counter";
-
-function CounterValue() {
-  const count = useCounter((state) => state.count);
-
-  return <div>Counter value is {count}</div>;
-}
-
-export default CounterValue;
