f13b4b4ec3690e9dc16d22bc41130ae094b9ee26
ravikesana1998 <ramuawsdevops1@gmail.com>
2025-04-18 11:27:02 +0530

    Delete 10. mern-stack-task-app-jwt directory

--- 10. mern-stack-task-app-jwt/server/database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(
-    "mongodb+srv://sangammukherjee2022:sangammukherjee2023@cluster0.fiyruuq.mongodb.net/"
-  )
-  .then(() => console.log("MongoDB Connection successfull"))
-  .catch((error) => console.log(`Error occured: ${error}`));
